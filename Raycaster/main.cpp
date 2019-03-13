c5c8ad9ba57547bfb27fd832fa95d8d9aa381ef8
Benjamin von Essen <benjamin@benjamins-mini.lan>
2019-03-13 10:40:17 +0100

    Initial Commit

---
+++ Raycaster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raycaster
+//
+//  Created by Benjamin von Essen on 2019-03-13.
+//  Copyright © 2019 Benjamin von Essen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
